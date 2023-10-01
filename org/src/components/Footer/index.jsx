642e87948656380aeb82546925182d76020f7c5b
Alfonso Díaz Celito <112213375+AlfonsoDiaz09@users.noreply.github.com>
2023-10-01 10:02:59 -0600

    Update index.jsx
    Se actualizó el target de las redes sociales

--- org/src/components/Footer/index.jsx
+++ org/src/components/Footer/index.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
         <footer className='footer' style={{backgroundImage: "url(/img/Footer.png)"}}>
             <div className='redes'>
                 <a href='https://www.facebook.com/AlfonsoDiazC.09/'>
-                    <img src='/img/facebook.png' alt='Facebook' />
+                    <img src='/img/facebook.png' target='_blank' alt='Facebook' />
                 </a>
                 <a href='https://twitter.com/Alfonsodiazc09'>
-                    <img src='/img/twitter.png' alt='Twitter' />
+                    <img src='/img/twitter.png' target='_blank' alt='Twitter' />
                 </a>
                 <a href='https://www.instagram.com/alfonsodiazc.09/'>
-                    <img src='/img/instagram.png' alt='Instagram' />
+                    <img src='/img/instagram.png' target='_blank' alt='Instagram' />
                 </a>
             </div>
             <img src='/img/Logo.png' alt='org'/>
