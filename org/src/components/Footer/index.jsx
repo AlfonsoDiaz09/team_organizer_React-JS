a9e3d497e5b1f06d97f6ac1d626c1c3b3a0587ac
Alfonso Díaz Celito <112213375+AlfonsoDiaz09@users.noreply.github.com>
2023-10-01 10:05:05 -0600

    Update index.jsx
    Actualizado target _blank de las redes sociales

--- org/src/components/Footer/index.jsx
+++ org/src/components/Footer/index.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
     return (
         <footer className='footer' style={{backgroundImage: "url(/img/Footer.png)"}}>
             <div className='redes'>
-                <a href='https://www.facebook.com/AlfonsoDiazC.09/'>
-                    <img src='/img/facebook.png' target='_blank' alt='Facebook' />
+                <a href='https://www.facebook.com/AlfonsoDiazC.09/' target='_blank'>
+                    <img src='/img/facebook.png' alt='Facebook' />
                 </a>
-                <a href='https://twitter.com/Alfonsodiazc09'>
-                    <img src='/img/twitter.png' target='_blank' alt='Twitter' />
+                <a href='https://twitter.com/Alfonsodiazc09' target='_blank'>
+                    <img src='/img/twitter.png' alt='Twitter' />
                 </a>
-                <a href='https://www.instagram.com/alfonsodiazc.09/'>
-                    <img src='/img/instagram.png' target='_blank' alt='Instagram' />
+                <a href='https://www.instagram.com/alfonsodiazc.09/' target='_blank'>
+                    <img src='/img/instagram.png' alt='Instagram' />
                 </a>
             </div>
             <img src='/img/Logo.png' alt='org'/>
